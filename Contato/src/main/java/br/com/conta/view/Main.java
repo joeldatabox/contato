b011d7903064a985764ca57b75b86fba09360115
Joel Rodrigues Moreira <joel.databox@gmail.com>
2016-08-12 21:59:56 -0300

    main

---
+++ Contato/src/main/java/br/com/conta/view/Main.java
@@ -0,0 +1,14 @@
+package br.com.conta.view;
+
+import br.com.contato.dao.ContatoCrud;
+import br.com.contato.dao.Crud;
+import br.com.contato.model.Contato;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[]args){
+        //teste aqui os metodos de crud que criamos
+    }
+}
